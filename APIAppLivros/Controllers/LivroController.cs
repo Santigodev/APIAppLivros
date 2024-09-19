05d468718686d4721296a21ca81c910a5b96cd13
Santigodev <shilton.santiago@gmail.com>
2024-09-19 20:25:12 -0300

    Atualizar e Criar novo Livro

--- APIAppLivros/Controllers/LivroController.cs
+++ APIAppLivros/Controllers/LivroController.cs
@@ -1,6 +1,7 @@
 ﻿using APIAppLivros.Models;
 using APIAppLivros.Repositories;
 using Microsoft.AspNetCore.Mvc;
+using MySqlX.XDevAPI;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -33,21 +34,28 @@ namespace APIAppLivros.Controllers
 
         // POST api/<LivroController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<IActionResult> Post([FromBody] Livro dados)
         {
+            await _livroRepository.SalvarDB(dados);
+
+            return Created("", dados);
         }
 
         // PUT api/<LivroController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public async Task<IActionResult> Put(int id, [FromBody] Livro dados)
         {
+            dados.Id = id;
+            await _livroRepository.AtualizarDB(dados);
+
+            return Ok();
         }
 
         // DELETE api/<LivroController>/5
         [HttpDelete("{id}")]
         public async Task<IActionResult> Delete(int id)
         {
-            await _livroRepository.DeletarPorid (id);
+            await _livroRepository.DeletarPoridDB (id);
 
             return  Ok();
         }
--- APIAppLivros/Repositories/LivroRepository.cs
+++ APIAppLivros/Repositories/LivroRepository.cs
@@ -37,13 +37,39 @@ namespace APIAppLivros.Repositories
             }
         }
 
-        public async Task<int> DeletarPorid(int id)
+        public async Task<int> DeletarPoridDB(int id)
         {
             using (var conn = Connection)
             {
                 var sql = "DELETE FROM tb_livros where Id = @id";
 
                 return await conn.ExecuteAsync(sql, new { Id = id });
+            }
+        }
+
+        public async Task<int> SalvarDB(Livro dados)
+        {
+            var sql = "INSERT INTO tb_livros (Titulo,Autor,AnoPublicacao,Genero,NumeroPaginas) VAlUES (@Titulo,@Autor,@AnoPublicacao,@Genero,@NumeroPaginas)";
+
+            using (var conn = Connection)
+            {
+                return await conn.ExecuteAsync(sql, new {
+                    Titulo = dados.Titulo,
+                    Autor = dados.Autor,
+                    AnoPublicacao = dados.AnoPublicacao,
+                    Genero = dados.Genero,
+                    NumeroPaginas = dados.NumPaginas
+                });
+            }
+        }
+
+        public async Task<int> AtualizarDB(Livro dados)
+        {
+            var sql = "UPDATE tb_livros SET Titulo = @Titulo, Autor = @Autor, AnoPublicacao = @AnoPublicacao,Genero = @Genero,NumeroPaginas = @NumPaginas WHERE Id = @Id";
+
+            using (var conn = Connection)
+            {
+                return await conn.ExecuteAsync(sql, dados);
             }
         }
     }
